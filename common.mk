c3851d86ffa8e8b03b6456ba2ef52eb621d7f02f
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-14 20:29:40 +0200

    sm8150-common: Add TriStateHandler for alert slider events.
    Change-Id: Ifb29f21ac28a413c2c9fcd2d358b8ce4545c26e8
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -389,7 +389,8 @@ PRODUCT_COPY_FILES += \
 
 # tri-state key
 PRODUCT_PACKAGES += \
-    tri-state-key_daemon
+    tri-state-key_daemon \
+    TriStateHandler
 
 # Update engine
 PRODUCT_PACKAGES += \
