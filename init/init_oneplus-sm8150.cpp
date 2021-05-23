5c10a6bc55f78ab9a9efb6daa985006d1cd4e27b
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-23 15:36:36 +0000

    sm8150-common: inital bringup for aospa
    * drop in-tree bootctrl and dependencies
    * drop unused flags
    * use usb component
    * update HIDL manifests for CAF
    * blob perf deps
    * drop non-existent overlays and general cleanup
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- init/init_oneplus-sm8150.cpp
+++ init/init_oneplus-sm8150.cpp
@@ -50,12 +50,6 @@ void property_override(char const prop[], char const value[]) {
     __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void property_override_multi(char const system_prop[], char const vendor_prop[],char const bootimage_prop[], char const value[]) {
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
-    property_override(bootimage_prop, value);
-}
-
 void load_dalvikvm_properties() {
   struct sysinfo sys;
   sysinfo(&sys);
@@ -200,7 +194,4 @@ void vendor_load_properties() {
 
   // dalvikvm props
   load_dalvikvm_properties();
-
-  // fingerprint
-  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ2A.210505.002/7246365:user/release-keys");
 }
